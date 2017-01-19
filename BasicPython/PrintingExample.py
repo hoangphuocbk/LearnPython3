5273e3ba2bade4e6ace56a01614a28d0bbb3546f
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-19 05:58:16 +0000

    add PrintingExample.py

---
+++ BasicPython/PrintingExample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("Hello World!")
+
+count = 5
+print("There are %1.2f people in %s room" %(count, "Bangolar"))
+
+print("One: {a}, Two: {b}, Three: {c}".format(a=1, c=3, b=2))
+
+print("{1} {2} {0}".format("ohh","one","two"))
