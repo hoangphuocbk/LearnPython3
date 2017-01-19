a1eac1eea02d9ef3634899acb98b9054065ddf40
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-19 08:35:05 +0000

    add some terrible things

--- FlowControl/function_For.py
+++ FlowControl/function_For.py
@@ -11,3 +11,26 @@ print(your_list)
 # loop each element by using for
 for i in your_list:
 	print(i)
+
+print("Done\n\n")
+print("############################################")
+
+SUFFIXS = {
+	1000: ['KB', 'MB', 'GB', 'TB', 'PB'],
+	1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB']
+}
+
+size = int(input("Put your HDD size in here: "))
+typeHDD = int(input("What is your unit, 1000(B) or 1024(iB)? "))
+
+value = size
+index = ''
+for i in SUFFIXS[typeHDD]:
+	value = value/typeHDD
+	if( value > typeHDD):
+		pass
+	else:
+		index = i
+		break
+
+print("Your HDD size: %1.3f %s" %(value, index))
