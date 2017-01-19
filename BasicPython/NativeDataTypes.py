2ae204b57dca7475e4eea13b40efd751f4d78b64
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-19 04:32:16 +0000

    Add basic knowledge Python

---
+++ BasicPython/NativeDataTypes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+## integer
+a = 1
+b = 0x10
+
+print(type(a))
+
+## float
+c = 3/2
+d = 3e2
+
+print(type(d))
+
+## complex
+e = 1 +2j
+f = complex(1,2)
+
+print(type(e))
+print(f==e)
---
+++ BasicPython/Operators.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print(1+1)
+
+print(10-5)
+
+print(2*3)
+
+print(5/2)
+
+print(5//2)
+
+print(5%2)
+
+print(2**3)
