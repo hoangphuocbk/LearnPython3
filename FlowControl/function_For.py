70594641e1929ab171817d1b13264b280e243248
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-19 07:51:26 +0000

    add an example for loop by using FOR

---
+++ FlowControl/function_For.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+your_str = input("Write your new sentence in there: ")
+
+your_list = your_str.split()
+
+# print list
+print(your_list)
+
+# loop each element by using for
+for i in your_list:
+	print(i)
