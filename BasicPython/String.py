7f8298efdab8f8291ae29ce6514c88d1090336e8
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-19 06:08:11 +0000

    add String.py

---
+++ BasicPython/String.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+s1 = "Welcome"
+s2 = "to"
+s3 = "Tarkan!"
+
+# print type of string s1
+print(type(s1))
+
+# print the sentence
+print("%s %s %s" %(s1,s2,s3))
+print(s1 + " " + s2 + " " +s3)
+print("{0} {1} {2}".format(s1,s2,s3))
+
+# Print length
+print(len(s3))
+
+# Slicing
+print(s1[:4])
