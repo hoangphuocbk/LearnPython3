dc3fcdc9a8045363d61d8490436f1eff0b558744
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-19 08:46:35 +0000

    Fix some bugs

--- FlowControl/function_For.py
+++ FlowControl/function_For.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+
+# The first example
+
+''' 
 your_str = input("Write your new sentence in there: ")
 
 your_list = your_str.split()
@@ -11,6 +16,8 @@ print(your_list)
 # loop each element by using for
 for i in your_list:
 	print(i)
+'''
+
 
 print("Done\n\n")
 print("############################################")
@@ -20,17 +27,26 @@ SUFFIXS = {
 	1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB']
 }
 
-size = int(input("Put your HDD size in here: "))
-typeHDD = int(input("What is your unit, 1000(B) or 1024(iB)? "))
+try:
+	size = int(input("Put your HDD size in here: "))
+except ValueError:
+	print("You put the size number that is not number")
+
+try:
+	typeHDD = int(input("What is your unit, 1000(B) or 1024(iB)? "))
+	if((typeHDD != 1000) and (typeHDD != 1024)):
+		sys.exit("Error type of your HDD")
+except ValueError:
+	print("You put type of HDD that is not number")
 
 value = size
 index = ''
 for i in SUFFIXS[typeHDD]:
 	value = value/typeHDD
-	if( value > typeHDD):
+	if( value >= typeHDD):
 		pass
 	else:
 		index = i
 		break
 
-print("Your HDD size: %1.3f %s" %(value, index))
+print("Your HDD size: %1.1f %s" %(value, index))
