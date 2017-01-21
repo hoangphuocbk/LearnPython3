6dbc19f9b18d307608641d4af9437157e597076d
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-22 00:14:16 +0700

    change install-MySQLPython.sh script to meet python3.4

--- MySQLDatabase/install/install-MySQLPython.sh
+++ MySQLDatabase/install/install-MySQLPython.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
-# Install pip first
-if hash pip 2>/dev/null;
-then
-	echo "You already installed python-pip"
-else
-	echo "You don't install python-pip"
-	echo "Do you want to install it? (y/n)"
-	read input_var
-	if [ $input_var == "y" ]
-		then
-			sudo apt-get install -y python-pip
-	fi
-fi
+# Install python-mysql-connector
+wget https://cdn.mysql.com//Downloads/Connector-Python/mysql-connector-python-py3_2.1.5-1ubuntu14.04_all.deb
 
-# Install necessary packages
-sudo apt-get -y install python-dev libmysqlclient-dev
-sudo pip install wheel
+sudo dpkg -i PACKAGE.deb
 
-# Install MySQL connector for python
-sudo pip install --egg -r ./requirements.txt
-
-if [ $? == 0 ]
-then
-	echo "MySQL connector was installed successfully~"
-else
-	echo "Installing MySQL connector was failed~"
-fi
+# Install mysql-client
+sudo apt-get install -y mysql-client
