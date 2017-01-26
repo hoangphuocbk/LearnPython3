d705e552fa4c652f024c1c2205305f8b2954f460
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-01-26 23:44:12 +0700

    follow tutorial :((

--- PythonAdvanced/finite_state_M/StateMachine.py
+++ PythonAdvanced/finite_state_M/StateMachine.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+class StateMachine:
+	def __init__(self):
+		self.handlers = {}
+		self.startState = None
+		self.endStates = []
+	def add_state(self, name, handler, end_state):
+		name = name.upper()
+		self.handlers[name] = handler
+		if end_state:
+			self.endStates.append(name)
+	def set_start(self, name):
+		self.startState = name.upper()
+	def run(self, cargo):
+		try:
+			handler = self.handlers[self.startState]
+		except:
+			raise InitializationError("must call set_start() before run()")
+		if not self.endStates:
+			raise InitializationError("at least one start is end state")
+		while True:
+			newState = handler(cargo)
+			if newState.upper() in self.endStates:
+				print("reached", newState)
+				break
+			else:
+				handler = self.handlers[newState.upper()]
