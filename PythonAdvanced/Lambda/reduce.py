7a0f904f019e3d3040c0c041d84a66463c5abc86
Hoang Cong Phuoc <hoangphuocbk2.07@gmail.com>
2017-02-03 02:53:40 +0000

    add reduce() example

---
+++ PythonAdvanced/Lambda/reduce.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import functools
+
+# lambda function to find out the maximum
+max = lambda x, y: x if x > y else y
+
+if __name__ == '__main__':
+	# list example
+	list = [12, 34, 1, 45, 78, 132]
+	
+	print(functools.reduce(max, list))
